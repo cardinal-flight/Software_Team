0436824dc915081f2ed61e118142b1a16301cd9f
Alex Scott <alscott@iastate.edu>
2019-03-25 16:25:49 -0500

    Update Simple_Simulation.py
    Math now works

--- Simple_Simulation.py
+++ Simple_Simulation.py
@@ -10,7 +10,7 @@ import time
 clr.AddReference("MissionPlanner")
 import MissionPlanner
 clr.AddReference("MissionPlanner.Utilities") #Includes utilities class
-# from MissionPlanner.Utilities import Locationwp
+from MissionPlanner.Utilities import Locationwp
 # #Last 4 lines of code are straight copied
 #
 def distance(lat1, lon1, lat2, lon2):
@@ -25,71 +25,48 @@ def distance(lat1, lon1, lat2, lon2):
     dLat = lat2 - lat1
     dLon = lon2 - lon1
     #
-    a = math.sin(0.5 * dLat)**2 + math.sin(0.5 * dLon)**2 math.cos(lat1) * math.cos(lat2) #It doesn't like this line!
-    # c = 2 * math.atan2(sqrt(a), math.sqrt(1 - a))
-    c = .003
+    a = math.sin(0.5 * dLat)**2 + math.sin(0.5 * dLon)**2 + math.cos(lat1) * math.cos(lat2) #It doesn't like this line!
+    c = 2 * math.atan2(sqrt(a), math.sqrt(1 - a))
     return r_e * c
 
-print 'Start Script'
-for chan in range(1,9):
-    Script.SendRC(chan,1500,False)
-    Script.SendRC(3,Script.GetParam('RC3_MIN'),True)
+print 'Running Script'
 
-Script.Sleep(5000)
+Script.ChangeMode("Guided") #Changed flight to guided
+item = MissionPlanner.Utilities.Locationwp() #creating waypoint
 
-while cs.lat == 0:
-    print 'Waiting for GPS'
-    Script.Sleep(1000)
+dist_tolerance = 15 #(m)
+ber_tolerance = 45 #heading tolerance
+#Both are something used in an example code
 
-print 'Got GPS'
-jo = 10 * 13
-print jo
-Script.SendRC(3,1000,False)
-Script.SendRC(4,2000,True)
-cs.messages.Clear()
-Script.WaitFor('ARMING MOTORS',30000)
-Script.SendRC(4,1500,True)
-print 'Motors Armed!'
-Script.SendRC(3,1700,True)
-#
-# print 'Running Script'
-#
-# Script.ChangeMode("Guided") #Changed flight to guided
-# item = MissionPlanner.Utilities.Locationwp() #creating waypoint
-#
-# dist_tolerance = 15 #(m)
-# ber_tolerance = 45 #heading tolerance
-# #Both are something used in an example code
-#
-# alt = 100.0000 #altitude in meters
-# Location.lat.SetValue(item, 25) #sets latitude
-# Location.lng.SetValue(item, 135) #sets longitude
-# Location.alt.SetValue(item, alt) #sets altitude
-# print "Target Set"
-#
-# MAV.setGuidedModeWP(item) #Tells UAV "go to" the set lat/long @ alt
-# print "Target in Sight"
-#
-# Good = True
-# goal_dist = distance(0, 0, math.radians(25), math.radians(135))
-#
-# while Good == True:
-#     speed_g = cs.groundspeed
-#     alt = cs.alt
-#     wp_dist = distance(cs.lat, cs.lng, math.radians(25), math.radians(135))
-#     print wp_dist
-#     ber_error = cs.ber_error
-#
-#     if(math.fabs(goal_dist - wp_dist) <= dist_tolerance):
-#         if (math.fabs(ber_error) <= ber_tolerance):
-#             print "Fire in the hole"
-#             break
-#         else:
-#             print "Heading outside of threshold, go around!"
-#             Good = False
-#
-#     else:
-#         print 'Outside of threshold.'
-#         time.sleep(1.0)
-#
-#     print 'Issue with making it to target'
+alt = 100.0000 #altitude in meters
+Locationwp.lat.SetValue(item, 25) #sets latitude
+Locationwp.lng.SetValue(item, 135) #sets longitude
+Locationwp.alt.SetValue(item, alt) #sets altitude
+print "Target Set"
+
+MAV.setGuidedModeWP(item) #Tells UAV "go to" the set lat/long @ alt
+print "Target in Sight"
+
+Good = True
+goal_dist = distance(0, 0, math.radians(25), math.radians(135))
+
+while Good == True:
+    speed_g = cs.groundspeed
+    alt = cs.alt
+    wp_dist = distance(cs.lat, cs.lng, math.radians(25), math.radians(135))
+    print wp_dist
+    ber_error = cs.ber_error
+
+    if(math.fabs(goal_dist - wp_dist) <= dist_tolerance):
+        if (math.fabs(ber_error) <= ber_tolerance):
+            print "Fire in the hole"
+            break
+        else:
+            print "Heading outside of threshold, go around!"
+            Good = False
+
+    else:
+        print 'Outside of threshold.'
+        time.sleep(1.0)
+
+    print 'Issue with making it to target'
